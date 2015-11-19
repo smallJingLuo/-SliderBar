7838868998a6f78c0bd1b927e04968db72dc7ce2
成都千锋 <1115658829@qq.com>
2015-11-19 13:13:44 +0800

    Initial Commit

---
+++ aiyuke/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aiyuke
+//
+//  Created by 成都千锋 on 15/11/19.
+//  Copyright (c) 2015年 成都千锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aiyuke/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  aiyuke
+//
+//  Created by 成都千锋 on 15/11/19.
+//  Copyright (c) 2015年 成都千锋. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
